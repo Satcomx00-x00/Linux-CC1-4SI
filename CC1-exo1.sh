b934457d2be81f64b6f6b0574642b4ef464fed02
satcom <raspsatcombot@gmail.com>
2024-03-04 11:50:51 +0000

    final

--- CC1-exo1.sh
+++ CC1-exo1.sh
@@ -2,7 +2,6 @@
 # Auteur: EG
 # Date: 2024-04-03
 # Description: Exo 1 du CC1
-# Binaires autorisées :
 
 classe=""                                                      # variable pour stocker le nom de la classe
 foldersToCreate=("Francais" "Histoire" "Mathematiques" "Docs") # tableau pour stocker les noms des dossiers à créer
@@ -21,7 +20,7 @@ fi
 # fonction pour créer les dossiers
 createFolders() {
     for folder in "${foldersToCreate[@]}"; do # pour chaque nom et prénom, créer un dossier avec le nom et le prénom (col2 _ col3)
-        mkdir -p "$classe/$1 $2/$folder"      # créer les dossiers avec le nom et le prénom (col2 _ col3) et créer des sous-dossiers 'Français', 'Histoire', 'Mathematiques', 'Docs'.
+        mkdir -p "$3/$1 $2/$folder"           # créer les dossiers avec le nom et le prénom (col2 _ col3) et créer des sous-dossiers 'Français', 'Histoire', 'Mathematiques', 'Docs'.
     done                                      # mkdir -p permet de créer les dossiers parents si ils n'existent pas
 }
 
@@ -37,6 +36,6 @@ do
         continue
     else
         echo -e "Création des dossiers pour $col2 $col3 dans la classe => $classe"
-        createFolders "$col2" "$col3" # appeler la fonction createFolders avec les valeurs de col2 et col3
+        createFolders "$col2" "$col3" "$classe" # appeler la fonction createFolders avec les valeurs de col2, col3 et classe
     fi
 done <listeEtudiants.csv
