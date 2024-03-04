ccbb393257924add64414e45d0a7a902cda65c3f
satcom <raspsatcombot@gmail.com>
2024-03-04 11:45:06 +0000

    update

--- CC1-exo1.sh
+++ CC1-exo1.sh
@@ -6,8 +6,27 @@
 
 classe=""                                                      # variable pour stocker le nom de la classe
 foldersToCreate=("Francais" "Histoire" "Mathematiques" "Docs") # tableau pour stocker les noms des dossiers à créer
+csvFile="listeEtudiants.csv"                                   # variable pour stocker le nom du fichier
 
-# méthode de lecture du fichier listeEtudiants.csv, IFS est utilisé pour définir le séparateur de colonnes et on défini les variables col1, col2 et col3 pour stocker les valeurs de chaque colonne selectionnées
+# tester si le fichier listeEtudiants.csv existe si oui tester si le fichier est vide
+if [ ! -f "$csvFile" ]; then # tester si le fichier listeEtudiants.csv existe
+    echo "Le fichier $csvFile n'existe pas"
+    exit 1
+fi
+if [ ! -s "$csvFile" ]; then # tester si le fichier listeEtudiants.csv est vide
+    echo "Le fichier $csvFile est vide"
+    exit 2
+fi
+
+# fonction pour créer les dossiers
+createFolders() {
+    for folder in "${foldersToCreate[@]}"; do # pour chaque nom et prénom, créer un dossier avec le nom et le prénom (col2 _ col3)
+        mkdir -p "$classe/$1 $2/$folder"      # créer les dossiers avec le nom et le prénom (col2 _ col3) et créer des sous-dossiers 'Français', 'Histoire', 'Mathematiques', 'Docs'.
+    done                                      # mkdir -p permet de créer les dossiers parents si ils n'existent pas
+}
+
+# méthode de lecture du fichier listeEtudiants.csv,
+# IFS est utilisé pour définir le séparateur de colonnes et on défini les variables col1, col2 et col3 pour stocker les valeurs de chaque colonne respectivement
 while
     IFS=';'
     read -r col1 col2 col3
@@ -18,9 +37,6 @@ do
         continue
     else
         echo -e "Création des dossiers pour $col2 $col3 dans la classe => $classe"
-        for folder in "${foldersToCreate[@]}"; do  # pour chaque nom et prénom, créer un dossier avec le nom et le prénom (col2 _ col3)
-            mkdir -p "$classe/$col2 $col3/$folder" # créer les dossiers avec le nom et le prénom (col2 _ col3) et créer des sous-dossiers 'Français', 'Histoire', 'Mathematiques', 'Docs'.
-        done                                       # mkdir -p permet de créer les dossiers parents si ils n'existent pas
+        createFolders "$col2" "$col3" # appeler la fonction createFolders avec les valeurs de col2 et col3
     fi
-
 done <listeEtudiants.csv
